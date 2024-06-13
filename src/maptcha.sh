60cfe03e72c899ce91e5ca5b2f2aeb41c0934292
Oieswarya <39045377+Oieswarya@users.noreply.github.com>
2024-06-13 11:39:57 -0700

    Update maptcha.sh

--- src/maptcha.sh
+++ src/maptcha.sh
@@ -40,7 +40,7 @@ python3 ~/Maptcha/src/Wiring.py "$map_output" ~/Maptcha/graphWH.txt "$HOME/Maptc
 python3 ~/Maptcha/src/PathEnumeration.py "$map_output" $HOME/Maptcha/wired_output.txt $HOME/Maptcha/path_output.txt
 python3 $HOME/Maptcha/src/CreateCCFileForEachPair_args.py "$HOME/Maptcha/wired_output.txt" "$HOME/Maptcha/graphLRID.txt" "$HOME/Maptcha/Output/"
 python3 $HOME/Maptcha/src/CreateBatchOfContigs.py "$HOME/Maptcha/path_output.txt" "$HOME/Maptcha/Output/" "$contigs_input_file" "$long_reads_input_file" "$HOME/Maptcha/Output/FastaFilesBatch_8192/" 40 8192
-
+python3 $HOME/Maptcha/Hifiasm/hifiasm.py "$HOME/Maptcha/path_output.txt" "$HOME/Maptcha/Output/" "$contigs_input_file" "$long_reads_input_file" "$HOME/Maptcha/Output/FastaFilesBatch_8192/" 40 8192
 ## Define compute options for the main job
 #PBS -l nodes=4:amd:ppn=2
 #PBS -l mem=4gb
